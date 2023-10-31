2f2cb00de89e58f824820dab8798553011d8ae8e
Shashank Naik <shashanknaik808@gmail.com>
2023-10-31 11:52:42 +0530

    Implemented an event listener for option images that highlights the selected option while deactivating others when clicked.

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const gameContainer = document.querySelector(".container"),
     optionImages = document.querySelectorAll(".option_image");
 
 console.log(gameContainer, userResult, cpuResult, result, optionImages);
+
+
+optionImages.forEach((image, index) => {
+    image.addEventListener("click", (e) => {
+        image.classList.add("active");
+
+        optionImages.forEach((image2, index2) => {
+            index !== index2 && image2.classList.remove("active");
+        });
+    });
+});
